518e6c0923806fdba7dc40798101840ccd533f64
pashpashpash <nik@nugbase.com>
2023-06-17 17:54:18 -0700

    increased size limitations

--- vault-web-server/postapi/fileupload.go
+++ vault-web-server/postapi/fileupload.go
@@ -18,8 +18,8 @@ type UploadResponse struct {
 	FailedFileNames     map[string]string `json:"failed_file_names"`
 }
 
-const MAX_FILE_SIZE int64 = 3 << 20         // 3 MB
-const MAX_TOTAL_UPLOAD_SIZE int64 = 3 << 20 // 3 MB
+const MAX_FILE_SIZE int64 = 25 << 20         // 3 MB
+const MAX_TOTAL_UPLOAD_SIZE int64 = 50 << 20 // 3 MB
 
 func (ctx *HandlerContext) UploadHandler(w http.ResponseWriter, r *http.Request) {
 
